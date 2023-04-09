ca9e2a1115ca31da2f24ac8c8bcfa1786fe231b7
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-09 18:59:28 +0200

    create Carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {ExploreTopBooks} from "./layouts/HomePage/ExploreTopBooks";
+import {Carousel} from "./layouts/HomePage/Carousel";
 
 function App() {
-  return (
-      <div>
-        <Navbar/>
-        <ExploreTopBooks/>
-      </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <ExploreTopBooks/>
+            <Carousel/>
+        </div>
+    );
 }
 
 export default App;
