a99be218e483757d06d30503eeed29c6f343fc34
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-09 19:52:38 +0200

    refactoring: create HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
-import {ExploreTopBooks} from "./layouts/HomePage/ExploreTopBooks";
-import {Carousel} from "./layouts/HomePage/Carousel";
-import {Hero} from "./layouts/HomePage/Hero";
-import {LibraryServices} from "./layouts/HomePage/LibraryServices";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
+import {HomePage} from "./layouts/HomePage/HomePage";
 
-function App() {
+export const App = () => {
     return (
         <div>
             <Navbar/>
-            <ExploreTopBooks/>
-            <Carousel/>
-            <Hero/>
-            <LibraryServices/>
+            <HomePage/>
             <Footer/>
         </div>
     );
 }
-
-export default App;
