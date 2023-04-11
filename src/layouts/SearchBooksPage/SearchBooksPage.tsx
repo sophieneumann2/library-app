6219fe1da69975f6cbb6fa594c5f0ca81f009e49
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-11 15:28:41 +0200

    fix pagination bug

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -133,13 +133,25 @@ export const SearchBooksPage = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={"mt-3"}>
-                        <h5>Number of results: ({totalAmountOfBooks})</h5>
-                        <p>{indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:</p>
-                        {books.map(book => <SearchBook book={book} key={book.id}/>)}
-                        {totalPages > 1 ?
-                            <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/> : null}
-                    </div>
+                    {totalAmountOfBooks > 0 ?
+                        <>
+                            <div className={"mt-3"}>
+                                <h5>Number of results: ({totalAmountOfBooks})</h5>
+                            </div>
+
+                            <p>{indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:</p>
+                            {books.map(book => <SearchBook book={book} key={book.id}/>)}
+                        </>
+                        :
+                        <div className={"m-5"}>
+                            <h3>Can't find what you're looking for?</h3>
+                            <a type={"button"} className={"btn main-color btn-md px-4 me-md-2 fw-bold text-white"}
+                               href={"#"}>Library Services</a>
+                        </div>
+                    }
+                    {totalPages > 1 ?
+                        <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/> : null}
+
                 </div>
             </div>
         </div>
