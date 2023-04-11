4a821269a3aad680d4258adcdaafffd8b7db66d6
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-11 14:23:10 +0200

    update util Pagination

--- src/layouts/Utils/Pagination.tsx
+++ src/layouts/Utils/Pagination.tsx
@@ -1,25 +1,29 @@
-import React from "react";
-
-export const Pagination: React.FC<{ currentPage: number, totalPages: number, paginate: any }> = (props) => {
+export const Pagination: React.FC<{
+    currentPage: number,
+    totalPages: number,
+    paginate: any
+}> = (props) => {
 
     const pageNumbers = [];
 
     if (props.currentPage === 1) {
         pageNumbers.push(props.currentPage);
         if (props.totalPages >= props.currentPage + 1) {
-            pageNumbers.push(props.currentPage + 1)
+            pageNumbers.push(props.currentPage + 1);
         }
         if (props.totalPages >= props.currentPage + 2) {
-            pageNumbers.push(props.currentPage + 2)
+            pageNumbers.push(props.currentPage + 2);
         }
     } else if (props.currentPage > 1) {
         if (props.currentPage >= 3) {
             pageNumbers.push(props.currentPage - 2);
-            pageNumbers.push(props.currentPage - 1)
+            pageNumbers.push(props.currentPage - 1);
         } else {
-            pageNumbers.push(props.currentPage - 1)
+            pageNumbers.push(props.currentPage - 1);
         }
+
         pageNumbers.push(props.currentPage);
+
         if (props.totalPages >= props.currentPage + 1) {
             pageNumbers.push(props.currentPage + 1);
         }
@@ -29,21 +33,23 @@ export const Pagination: React.FC<{ currentPage: number, totalPages: number, pag
     }
 
     return (
-        <nav aria-label={"..."}>
-            <ul className={"pagination"}>
-                <li className={"page-item"} onClick={() => props.paginate(1)}>
-                    <button className={"page-link"}>
+        <nav aria-label="...">
+            <ul className='pagination'>
+                <li className='page-item' onClick={() => props.paginate(1)}>
+                    <button className='page-link'>
                         First Page
                     </button>
-                    {pageNumbers.map(number => (
-                        <li key={number} onClick={() => props.paginate(number)}
-                            className={"page-item" + (props.currentPage === number ? "active" : "")}>
-                            <button className={"page-link"}>{number}</button>
-                        </li>
-                    ))}
                 </li>
-                <li className={"page-item"} onClick={() => props.paginate(props.totalPages)}>
-                    <button className={"page-link"}>
+                {pageNumbers.map(number => (
+                    <li key={number} onClick={() => props.paginate(number)}
+                        className={'page-item ' + (props.currentPage === number ? 'active' : '')}>
+                        <button className='page-link'>
+                            {number}
+                        </button>
+                    </li>
+                ))}
+                <li className='page-item' onClick={() => props.paginate(props.totalPages)}>
+                    <button className='page-link'>
                         Last Page
                     </button>
                 </li>
