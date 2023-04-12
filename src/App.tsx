87c84088b0dcbdea130c8105031f0f3bde87a4b8
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-12 08:03:17 +0200

    added Routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './App.css';
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
 import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./layouts/HomePage/HomePage";
 
 export const App = () => {
     return (
         <div>
             <Navbar/>
-            {/*<HomePage/>*/}
-            <SearchBooksPage/>
+            <Routes>
+                <Route path={"/"} element={<HomePage/>}/>
+                <Route path={"/home"} element={<HomePage/>}/>
+                <Route path={"/search"} element={<SearchBooksPage/>}/>
+            </Routes>
             <Footer/>
         </div>
     );
