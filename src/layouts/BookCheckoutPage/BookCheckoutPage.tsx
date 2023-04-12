f7b190cb280eb094bf307c7b917a936073e805d4
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-12 14:45:39 +0200

    add util Reviews, add component LatestReviews

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -4,6 +4,7 @@ import {SpinnerLoading} from "../Utils/SpinnerLoading";
 import {StarsReview} from "../Utils/StarsReview";
 import {CheckoutAndReviewBox} from "./CheckoutAndReviewBox";
 import ReviewModel from "../../models/ReviewModel";
+import {LatestReviews} from "./LatestReviews";
 
 export const BookCheckoutPage = () => {
 
@@ -123,11 +124,12 @@ export const BookCheckoutPage = () => {
                             <h5 className={"text-primary"}>{book?.author}</h5>
                             <p className={"lead"}>{book?.description}</p>
                         </div>
-                        <StarsReview rating={3.5} size={32}/>
+                        <StarsReview rating={totalStars} size={32}/>
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false}/>
                 </div>
                 <hr/>
+                <LatestReviews reviews={reviews} bookId={book?.id} mobile={false}/>
             </div>
             <div className={"container d-lg-none mt-5"}>
                 <div className={"d-flex justify-content-center align-items-center"}>
@@ -142,11 +144,12 @@ export const BookCheckoutPage = () => {
                         <h2>{book?.title}</h2>
                         <h5 className={"text-primary"}>{book?.author}</h5>
                         <p className={"lead"}>{book?.description}</p>
-                        <StarsReview rating={3.5} size={32}/>
+                        <StarsReview rating={totalStars} size={32}/>
                     </div>
                 </div>
                 <CheckoutAndReviewBox book={book} mobile={true}/>
                 <hr/>
+                <LatestReviews reviews={reviews} bookId={book?.id} mobile={true}/>
             </div>
         </div>
     );
