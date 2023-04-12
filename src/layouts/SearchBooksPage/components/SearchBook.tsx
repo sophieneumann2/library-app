2cde7a766e8dd560d57bcb8499bfe37cdb094c51
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-12 09:07:01 +0200

    add routing for BookCheckoutPage

--- src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -1,4 +1,6 @@
 import BookModel from "../../../models/BookModel";
+import {Link} from "react-router-dom";
+import {ROUTES} from "../../../properties";
 
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
     return (
@@ -32,7 +34,8 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
                     </div>
                 </div>
                 <div className={"col-md-4 d-flex justify-content-center align-items-center"}>
-                    <a className={"btn btn-md main-color text-white"} href={"#"}>View Details</a>
+                    <Link className={"btn btn-md main-color text-white"} to={`${ROUTES.CHECKOUT}/${props.book.id}`}>View
+                        Details</Link>
                 </div>
             </div>
         </div>
