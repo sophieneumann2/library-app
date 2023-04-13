8079a37b08e09b9e0ed6b89dabfd7c73d2df6834
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-13 09:04:19 +0200

    add Sign out functionality

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import {ROUTES} from "../../properties";
+import {useOktaAuth} from "@okta/okta-react";
+import {SpinnerLoading} from "../Utils/SpinnerLoading";
 
 export const Navbar = () => {
+
+    const {oktaAuth, authState} = useOktaAuth();
+
+    if (!authState) {
+        return <SpinnerLoading/>
+    }
+
+    const handleLogout = async () => oktaAuth.signOut();
+
     return (
         <nav className={"navbar navbar-expand-lg navbar-dark main-color py-3"}>
             <div className={"container-fluid"}>
                 <span className={"navbar-brand"}>Luv 2 Read</span>
-                <button className={"navbar-toggler"} type={"button"} data-bs-toggle={"collapse"} data-bs-target={"#navbarNavDropdown"} aria-controls={"navbarNavDropdown"} aria-expanded={"false"} aria-label={"Toggle Navigation"}>
+                <button className={"navbar-toggler"} type={"button"} data-bs-toggle={"collapse"}
+                        data-bs-target={"#navbarNavDropdown"} aria-controls={"navbarNavDropdown"}
+                        aria-expanded={"false"} aria-label={"Toggle Navigation"}>
                     <span className={"navbar-toggler-icon"}></span>
                 </button>
                 <div className={"collapse navbar-collapse"} id={"navbarNavDropdown"}>
@@ -20,9 +33,16 @@ export const Navbar = () => {
                         </li>
                     </ul>
                     <ul className={"navbar-nav ms-auto"}>
-                        <li className={"nav-item m1"}>
-                            <a type={"button"} className={"btn btn-outline-light"} href={"#"}>Sign in</a>
-                        </li>
+                        {authState.isAuthenticated ?
+                            <li>
+                                <button className={"btn btn-outline-light"} onClick={handleLogout}>Sign out</button>
+                            </li>
+                            :
+                            <li className={"nav-item m1"}>
+                                <Link type={"button"} className={"btn btn-outline-light"} to={ROUTES.LOGIN}>Sign
+                                    in</Link>
+                            </li>
+                        }
                     </ul>
                 </div>
             </div>
